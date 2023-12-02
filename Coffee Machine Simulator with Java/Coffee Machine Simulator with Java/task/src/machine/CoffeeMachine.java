b1405d109c12adba76f3b72e00ef2fa5198b2b7f
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-12-01 22:17:33 -0500

    Update CoffeeMachine.java

--- Coffee Machine Simulator with Java/Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -6,16 +6,30 @@ public class CoffeeMachine {
     public static void main(String[] args) {
 
         CoffeeMaker maker = new CoffeeMaker();
-        Drink espresso = new Drink(250, 0, 16, 4);
-        Drink latte = new Drink(350, 75, 20, 7);
-        Drink cappuccino = new Drink(200, 100, 12, 6);
+
 
         Scanner scanner = new Scanner(System.in);
 
+        maker.printSupplies();
+        System.out.println("Write action (buy, fill, take):");
+        switch (scanner.nextLine()) {
+            case "buy":
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                maker.buyDrink(scanner.nextInt());
+                break;
+            case "fill":
+                maker.fillMachine(scanner);
+                break;
+            case "take":
+                maker.takeMoney(scanner);
+                break;
+            default:
+                break;
+        }
+        maker.printSupplies();
 
 
     }
 
 
 }
-
