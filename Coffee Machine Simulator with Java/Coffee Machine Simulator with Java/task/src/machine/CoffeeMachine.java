3dc9ab0407f5f2c20cb326d12b1e9b529cc786bb
Christian Brewer <106411334+christian-the-brewer@users.noreply.github.com>
2023-12-01 22:27:30 -0500

    Update CoffeeMachine.java

--- Coffee Machine Simulator with Java/Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -5,28 +5,43 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
 
+        boolean menuIsOpen = true;
+        String userInput;
+
         CoffeeMaker maker = new CoffeeMaker();
 
 
         Scanner scanner = new Scanner(System.in);
 
-        maker.printSupplies();
-        System.out.println("Write action (buy, fill, take):");
-        switch (scanner.nextLine()) {
-            case "buy":
-                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-                maker.buyDrink(scanner.nextInt());
-                break;
-            case "fill":
-                maker.fillMachine(scanner);
-                break;
-            case "take":
-                maker.takeMoney(scanner);
-                break;
-            default:
-                break;
+        while (menuIsOpen) {
+            System.out.println("Write action (buy, fill, take):");
+            switch (scanner.nextLine()) {
+                case "buy":
+                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                    userInput = scanner.nextLine();
+                    if (userInput.equals("back")) {
+                        break;
+                    } else {
+                        maker.buyDrink(Integer.parseInt(userInput));
+                    }
+                    break;
+                case "fill":
+                    maker.fillMachine(scanner);
+                    break;
+                case "take":
+                    maker.takeMoney(scanner);
+                    break;
+                case "remaining":
+                    maker.printSupplies();
+                    break;
+                case "exit":
+                    menuIsOpen = false;
+                    break;
+                default:
+                    break;
+            }
         }
-        maker.printSupplies();
+
 
 
     }
