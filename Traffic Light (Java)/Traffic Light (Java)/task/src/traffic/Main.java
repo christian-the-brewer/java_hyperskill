66ee1c029f61bfbcd971a32ab1f85647d7f82364
Christian <brewer.christiang@gmail.com>
2023-12-03 17:39:28 -0500

    completes part 2 of project

--- Traffic Light (Java)/Traffic Light (Java)/task/src/traffic/Main.java
+++ Traffic Light (Java)/Traffic Light (Java)/task/src/traffic/Main.java
@@ -6,8 +6,8 @@ public class Main {
   public static void main(String[] args){
 
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Welcome to the traffic management system!");
-    System.out.println("Menu:\n1. Add\n2. Delete\n3. System\n0. Quit");
+    TrafficLight trafficLight = TrafficLight.setupSystem(scanner);
+    TrafficLight.menuSystem(trafficLight, scanner);
 
   }
 }
--- Traffic Light (Java)/Traffic Light (Java)/task/src/traffic/TrafficLight.java
+++ Traffic Light (Java)/Traffic Light (Java)/task/src/traffic/TrafficLight.java
@@ -18,21 +18,52 @@ public class TrafficLight {
         return interval;
     }
 
-    public void setNumberOfRoads(int numberOfRoads) {
-        this.numberOfRoads = numberOfRoads;
+    public void setNumberOfRoads(int roads) {
+        this.numberOfRoads = roads;
+    }
+
+    public void incrementRoads(int value) {
+        this.numberOfRoads += value;
     }
 
     public void setInterval(int interval) {
         this.interval = interval;
     }
 
-    static void setupSystem(Scanner scanner) {
+    static TrafficLight setupSystem(Scanner scanner) {
         System.out.println("Welcome to the traffic management system!");
         System.out.println("Input the number of roads: ");
         int roads = scanner.nextInt();
         System.out.println("Input the interval: ");
         int interval = scanner.nextInt();
 
-        TrafficLight trafficLight = new TrafficLight(roads, interval);
+        return new TrafficLight(roads, interval);
+    }
+
+    static void menuSystem(TrafficLight trafficLight, Scanner scanner) {
+        boolean menuOpen = true;
+        while (menuOpen) {
+            System.out.println("Menu:\n1. Add road\n2. Delete road\n3. Open System\n0. Quit");
+            int userInput = scanner.nextInt();
+            switch (userInput) {
+                case 1:
+                    trafficLight.incrementRoads(1);
+                    System.out.println("Road added");
+                    break;
+                case 2:
+                    trafficLight.incrementRoads(-1);
+                    System.out.println("Road deleted");
+                    break;
+                case 3:
+                    System.out.println("System opened");
+                    break;
+                case 0:
+                    menuOpen = false;
+                    System.out.println("Bye!");
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + userInput);
+            }
+        }
     }
 }
