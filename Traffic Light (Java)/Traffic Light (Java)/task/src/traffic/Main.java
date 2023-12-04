7984eeb23690806fef6364b0885856198e1c5351
Christian <brewer.christiang@gmail.com>
2023-12-04 15:00:38 -0500

    completes part 3

--- Traffic Light (Java)/Traffic Light (Java)/task/src/traffic/Main.java
+++ Traffic Light (Java)/Traffic Light (Java)/task/src/traffic/Main.java
@@ -1,13 +1,25 @@
 package traffic;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
     TrafficLight trafficLight = TrafficLight.setupSystem(scanner);
     TrafficLight.menuSystem(trafficLight, scanner);
 
   }
+
+  public static void clearScreen() {
+    try {
+      var clearCommand = System.getProperty("os.name").contains("Windows")
+              ? new ProcessBuilder("cmd", "/c", "cls")
+              : new ProcessBuilder("clear");
+      clearCommand.inheritIO().start().waitFor();
+    }
+    catch (IOException | InterruptedException e) {}
+  }
+
 }
--- Traffic Light (Java)/Traffic Light (Java)/task/src/traffic/TrafficLight.java
+++ Traffic Light (Java)/Traffic Light (Java)/task/src/traffic/TrafficLight.java
@@ -31,39 +31,78 @@ public class TrafficLight {
     }
 
     static TrafficLight setupSystem(Scanner scanner) {
+        int roads = 1, interval = 1;
         System.out.println("Welcome to the traffic management system!");
         System.out.println("Input the number of roads: ");
-        int roads = scanner.nextInt();
+        while (true) {
+            try {
+                roads = scanner.nextInt();
+                if (roads > 0) {
+                    break;
+                } else {
+                    System.out.println("Error! Incorrect Input. Try again: ");
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println("Error! Incorrect Input. Try again: ");
+                scanner.nextLine();
+            }
+        }
         System.out.println("Input the interval: ");
-        int interval = scanner.nextInt();
-
+        while (true) {
+            try {
+                interval = scanner.nextInt();
+                if (interval > 0) {
+                    break;
+                } else {
+                    System.out.println("Error! Incorrect Input. Try again: ");
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println("Error! Incorrect Input. Try again: ");
+                scanner.nextLine();
+            }
+        }
         return new TrafficLight(roads, interval);
     }
 
     static void menuSystem(TrafficLight trafficLight, Scanner scanner) {
         boolean menuOpen = true;
+        int userInput;
+        String buffer;
         while (menuOpen) {
             System.out.println("Menu:\n1. Add road\n2. Delete road\n3. Open System\n0. Quit");
-            int userInput = scanner.nextInt();
-            switch (userInput) {
-                case 1:
-                    trafficLight.incrementRoads(1);
-                    System.out.println("Road added");
-                    break;
-                case 2:
-                    trafficLight.incrementRoads(-1);
-                    System.out.println("Road deleted");
-                    break;
-                case 3:
-                    System.out.println("System opened");
-                    break;
-                case 0:
-                    menuOpen = false;
-                    System.out.println("Bye!");
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + userInput);
+            try {
+                userInput = scanner.nextInt();
+
+                switch (userInput) {
+                    case 1:
+                        trafficLight.incrementRoads(1);
+                        System.out.println("Road added");
+                        buffer = scanner.nextLine();
+                        break;
+                    case 2:
+                        trafficLight.incrementRoads(-1);
+                        System.out.println("Road deleted");
+                        buffer = scanner.nextLine();
+                        break;
+                    case 3:
+                        System.out.println("System opened");
+                        buffer = scanner.nextLine();
+                        break;
+                    case 0:
+                        menuOpen = false;
+                        System.out.println("Bye!");
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + userInput);
+                }
+            } catch (Exception e) {
+                System.out.println("Incorrect option");
+                buffer = scanner.nextLine();
+
             }
+            scanner.nextLine();
         }
     }
 }
